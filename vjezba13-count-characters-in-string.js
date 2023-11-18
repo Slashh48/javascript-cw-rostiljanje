3b5fdad353c58e1af1645e3d19da7025eba0baf5
Slashh48 <pablodiablofun@gmail.com>
2023-11-18 23:03:26 +0100

    Svi po carsiji me smarali aj Peki aj brate da mi popravis onaj kuler ja reko a viseee misim stvarno choveche

--- vjezba13-count-characters-in-string.js
+++ vjezba13-count-characters-in-string.js
@@ -1,5 +1,5 @@
-function count(str) {
-  return str.split("").reduce((total, letter) => {
+function count(string) {
+  return string.split("").reduce((total, letter) => {
     total[letter] ? total[letter]++ : (total[letter] = 1);
     return total;
   }, {});
